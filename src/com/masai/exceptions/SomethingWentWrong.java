c69653abe750aa790bdbca77314e8fafc5658298
Mhendrapratap <mahendrapratp354@gmail.com>
2023-03-31 23:16:32 +0530

    day-3

---
+++ src/com/masai/exceptions/SomethingWentWrong.java
@@ -0,0 +1,10 @@
+package com.masai.exceptions;
+
+public class SomethingWentWrong extends Exception{
+	public SomethingWentWrong() {
+		// TODO Auto-generated constructor stub
+	}
+	public SomethingWentWrong(String str) {
+		super(str);
+	}
+}
